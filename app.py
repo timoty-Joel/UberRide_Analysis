73af701c81657aa456656032e04373148a298975
Timoy <timotyjoel2811@gmail.com>
2025-11-12 02:38:08 +0700

    feat: KPI Visualization Part 1

--- app.py
+++ app.py
@@ -8,8 +8,18 @@ st.set_page_config(page_title='Uber Analysis Visualization')
 # --- Load Data from CSV File ---
 @st.cache_data()
 def load_data():
-    df = pd.read_csv('/data/uber_cleaned.csv')
-    df['date'] = pd.to_datetime('datetime')
+    try:
+        df = pd.read_csv("data/uber_cleaned.csv")
+    except:
+        st.error()
+        return 0
+    
+    
+    df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
+    df['Time'] = pd.to_datetime(df['Time'], format='%H:%M:%S', errors='coerce').dt.time
+    df['Day'] = df['Date'].dt.day_name()
+    df['Month'] = df['Date'].dt.month_name()
+    
     return df
 
 df = load_data()
@@ -30,3 +40,17 @@ vehicle_types = st.sidebar.multiselect(
     options=df['Vehicle Type'].unique(),
     default=df['Vehicle Type'].unique()
 )
+
+df_filtered = df[
+    (df['Date'].between(date_range[0], date_range[1])) &
+    (df['Vehicle Type'].isin(vehicle_types))
+]
+
+# -----------------
+# KPI Visualization
+# -----------------
+
+st.title("Uber Rides Analytic Dashboard")
+
+col1, col2, col3, col4 = st.columns(4)
+col1.metric("Total Rides: ", len(df_filtered['is_Completed']))
